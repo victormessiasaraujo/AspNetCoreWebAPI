f69ad405f13f7b26426feee60c77892e7f2af1b5
victormessiasaraujo <victor.messias.araujo@gmail.com>
2021-03-07 22:01:55 -0300

    Criação do dbContext

--- SmartSchool.WebAPI/Data/SmartContext.cs
+++ SmartSchool.WebAPI/Data/SmartContext.cs
@@ -1,7 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using SmartSchool.WebAPI.Models;
+
 namespace SmartSchool.WebAPI.Data
 {
-    public class SmartContext
+    public class SmartContext : DbContext
     {
-        
+        public SmartContext(DbContextOptions<SmartContext> options) : base(options){}
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Professor> Professores { get; set; }
+        public DbSet<Disciplina> Disciplinas { get; set; }
+        public DbSet<AlunoDisciplina> AlunosDisciplinas { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<AlunoDisciplina>()
+                .HasKey(AD => new {AD.AlunoId, AD.DisciplinaId});
+        }
     }
 }
