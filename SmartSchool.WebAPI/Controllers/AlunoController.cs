0a221d1d82480938ec84add31f436261e8e2dd34
victormessiasaraujo <victor.messias.araujo@gmail.com>
2021-03-10 22:19:43 -0300

    ###

--- SmartSchool.WebAPI/Controllers/AlunoController.cs
+++ SmartSchool.WebAPI/Controllers/AlunoController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using SmartSchool.WebAPI.Data;
 using SmartSchool.WebAPI.Models;
 
 namespace SmartSchool.WebAPI.Controllers
@@ -9,49 +10,26 @@ namespace SmartSchool.WebAPI.Controllers
     [Route("api/[controller]")]
     public class AlunoController : ControllerBase
     {
-        public List<Aluno> Alunos = new List<Aluno>()
+        private readonly SmartContext _context;
+        public AlunoController(SmartContext context) 
         {
-            new Aluno(){
-                Id = 1,
-                Nome = "João",
-                Sobrenome = "Paulo",
-                Telefone = "01010101010"
-            },
-            new Aluno(){
-                Id = 2,
-                Nome = "Maria",
-                Sobrenome = "Julia",
-                Telefone = "020202020202"
-            },
-            new Aluno(){
-                Id = 3,
-                Nome = "Rafael",
-                Sobrenome = "Medeiros",
-                Telefone = "0303030300303"
-            },
-            new Aluno(){
-                Id = 4,
-                Nome = "Paulo",
-                Sobrenome = "Melo",
-                Telefone = "0404040404040"
-            }
-        };
-        public AlunoController(){}
+            _context = context;
+        }
 
         [HttpGet]
-        public IActionResult Get ()
-        { 
-            return Ok(Alunos);
+        public IActionResult Get()
+        {
+            return Ok(_context.Alunos);
         }
 
         //passando parametro por rota
         //api/aluno/1
         [HttpGet("{id:int}")]
-        public IActionResult GetById (int id)
-        { 
-            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+        public IActionResult GetById(int id)
+        {
+            var aluno = _context.Alunos.FirstOrDefault(a => a.Id == id);
 
-            if(aluno == null)
+            if (aluno == null)
             {
                 return BadRequest("O aluno não foi encontrado");
             }
@@ -62,11 +40,11 @@ namespace SmartSchool.WebAPI.Controllers
         //passando parametro por queryString
         //api/aluno/byId?id=1
         [HttpGet("byId")]
-        public IActionResult GetByIdQuerystring (int id)
-        { 
-            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+        public IActionResult GetByIdQuerystring(int id)
+        {
+            var aluno = _context.Alunos.FirstOrDefault(a => a.Id == id);
 
-            if(aluno == null)
+            if (aluno == null)
             {
                 return BadRequest("O aluno não foi encontrado");
             }
@@ -78,10 +56,10 @@ namespace SmartSchool.WebAPI.Controllers
         //api/aluno/Maria
         [HttpGet("{nome}")]
         public IActionResult GetByNome(string nome)
-        { 
-            var aluno = Alunos.FirstOrDefault(a => a.Nome.Contains(nome));
+        {
+            var aluno = _context.Alunos.FirstOrDefault(a => a.Nome.Contains(nome));
 
-            if(aluno == null)
+            if (aluno == null)
             {
                 return BadRequest("O aluno não foi encontrado");
             }
@@ -93,12 +71,12 @@ namespace SmartSchool.WebAPI.Controllers
         //api/aluno/ByName?nome=Maria&sobrenome=Julia
         [HttpGet("ByName")]
         public IActionResult GetByNomeQueryString(string nome, string sobrenome)
-        { 
-            var aluno = Alunos.FirstOrDefault(a => 
+        {
+            var aluno = _context.Alunos.FirstOrDefault(a =>
                 a.Nome.Contains(nome) && a.Sobrenome.Contains(sobrenome)
             );
 
-            if(aluno == null)
+            if (aluno == null)
             {
                 return BadRequest("O aluno não foi encontrado");
             }
@@ -109,28 +87,28 @@ namespace SmartSchool.WebAPI.Controllers
         //api/aluno
         [HttpPost]
         public IActionResult Post(Aluno aluno)
-        { 
+        {
             return Ok(aluno);
         }
 
         //api/aluno
         [HttpPut("{id}")]
         public IActionResult Put(int id, Aluno aluno)
-        { 
+        {
             return Ok(aluno);
         }
 
         //api/aluno
         [HttpPatch("{id}")]
         public IActionResult Patch(int id, Aluno aluno)
-        { 
+        {
             return Ok(aluno);
         }
 
         //api/aluno
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
-        { 
+        {
             return Ok();
         }
     }
